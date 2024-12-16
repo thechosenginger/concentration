26eb2a5da1f389d8e8cb4e7c316ecc539a415ca6
thechosenginger <sewell.matthewm@gmail.com>
2024-12-16 10:10:33 -0600

    add webpack --watch cmd and components to app

--- apps/ui/src/app/app.tsx
+++ apps/ui/src/app/app.tsx
@@ -1,11 +1,17 @@
 // Uncomment this line to use CSS modules
 // import styles from './app.module.scss';
 import NxWelcome from './nx-welcome';
+import { Header, MemoryGrid } from './components';
 
 export function App() {
   return (
     <div>
-      <NxWelcome title="ui" />
+      {/* <NxWelcome title="ui" /> */}
+      <Header />
+      <MemoryGrid
+        buttonText="Memory Grid"
+        placeholder="Memory Button"
+      />
     </div>
   );
 }
---
+++ apps/ui/src/app/components/index.ts
@@ -0,0 +1,7 @@
+import Header from "./Header/header";
+import MemoryGrid from "./MemoryGrid/memory-grid";
+
+export {
+  Header,
+  MemoryGrid
+}
