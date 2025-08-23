d94a006560ad9a54dd602219647982cc0c73ed94
thechosenginger <sewell.matthewm@gmail.com>
2025-08-23 14:59:11 -0500

    setup jest and jest-dom for react testing

--- apps/ui/src/app/components/Header/header.spec.tsx
+++ apps/ui/src/app/components/Header/header.spec.tsx
@@ -1,3 +1,13 @@
-it('should include a basic test', () => {
-  expect(true).toBeTruthy();
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+
+import Header from './header';
+
+describe('Header Test Suite', () => {
+  beforeEach(() => render(<Header />));
+  it('should have a title', () => {
+    const h1Element = screen.getByRole('heading', { level: 1 });
+    expect(h1Element).toBeInTheDocument();
+    expect(h1Element).toHaveTextContent('Welcome to Concentration!!!');
+  });
 });
